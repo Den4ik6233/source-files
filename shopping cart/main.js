d1ee1155a570086ab694ebf3b8cad7852975ff98
Denis <44503422+Den4ik6233@users.noreply.github.com>
2022-02-23 16:48:12 +0300

    update

--- shopping cart/main.js
+++ shopping cart/main.js
@@ -1,36 +1,33 @@
 //1
-const price = document.getElementsByClassName('price');
-
-let num1 = price[0].innerText.replace(/[^0-9]/g, '');
-let num2 = price[1].innerText.replace(/[^0-9]/g, '');
-let num3 = price[2].innerText.replace(/[^0-9]/g, '');
+let price = document.querySelectorAll('div.price');
+const array = [];
 
 
-num1 = Number(num1);
-num2 = Number(num2);
-num3 = Number(num3);
+for (let i of price) {
+  array.push(i.innerText.slice(0, -1).split(" ").join() *1);
+}
 
-const sum = num1 + num2 + num3;
+let result = array.reduce(function(sum ,i){
+  return sum += i;
+});
 
 function numSum() {
-document.querySelector('.total').innerHTML = sum;
+document.querySelector('.total').innerHTML = result;
 }
 
 //2
 
-const oldPrice = document.getElementsByClassName('old-price');
+let oldPrice = document.querySelectorAll('div.old-price');
+const array2 = [];
 
-let num4 = oldPrice[0].innerText.replace(/[^0-9]/g, '');
-let num5 = oldPrice[1].innerText.replace(/[^0-9]/g, '');
-let num6 = oldPrice[2].innerText.replace(/[^0-9]/g, '');
+for (let i of oldPrice) {
+  array2.push(i.innerText.slice(0, -1).split(" ").join() *1);
+}
 
-
-num4 = Number(num4);
-num5 = Number(num5);
-num6 = Number(num6);
-
-const sum2 = num4 + num5 + num6;
+let result2 = array2.reduce(function(sum ,i){
+  return sum += i;
+});
 
 function numSum2() {
-document.querySelector('.totalWithoutSale').innerHTML = sum2;
+document.querySelector('.totalWithoutSale').innerHTML = result2;
 }
